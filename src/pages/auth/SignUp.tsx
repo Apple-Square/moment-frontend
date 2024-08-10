8e64d9a9e3ff56191c746a5d6976162c27f4a5aa
HyeonJun0527 <osumaniaddict527@gmail.com>
2024-08-10 23:17:25 +0900

    feat:회원가입 화면 제작 회원가입 화면의 30%정도를 제작했다. Related To:#2

--- src/pages/auth/SignUp.tsx
+++ src/pages/auth/SignUp.tsx
@@ -1,22 +1,77 @@
 import React from "react";
-import {Form, Col, Container, Row} from "react-bootstrap";
+import {Form, Col, Container, Row, Button} from "react-bootstrap";
 import st from "./css/signUp.module.css"
+import MomentLogoNTextImg from "./component/MomentLogoNTextImg.tsx";
+import {IoMdArrowRoundBack} from "react-icons/io";
 export const SignUp:React.FC = () => {
     return (
         <Container className={`${st.container}`}>
-            <Row className={`w-100 mb-4 ${st.h8}`} style={{minWidth: "300px"}}>
-                <Col>
-                    <Form.Control type="text" placeholder="닉네임"
-                                className={`mb-3 ${st.h100}`}/>
+            <Row className="d-flex justify-content-between align-items-center w-100 mb-4">
+                <Col xs="auto" style={{height:"auto"}}>
+                    <div
+                        className={`${st.backButton}`}
+                        onClick={() => window.history.back()}>
+                        <IoMdArrowRoundBack size={32}/>
+                    </div>
+                </Col>
+                <Col xs="auto" style={{maxWidth:"80px"}}>
+                    <MomentLogoNTextImg />
                 </Col>
             </Row>
-            <Row>
-                <Col className="d-flex justify-content-center">
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="닉네임"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
                     <Form.Control type="text" placeholder="아이디"
                                 className={`mb-3 ${st.h100}`}/>
                 </Col>
             </Row>
-            뿅
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="비밀번호"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="비밀번호 재입력"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="생년월일 이거는 datePicker"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="성별 이거도 선택으로"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="이메일 이거 입력 바꾸기 @와 선택으로"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 mb-2 ${st.h8}`} style={{minWidth: "300px"}}>
+                <Col>
+                    <Form.Control type="text" placeholder="주소지 얘 주소라이브러리"
+                                  className={`mb-3 ${st.h100}`}/>
+                </Col>
+            </Row>
+            <Row className={`w-100 ${st.h8}`}  style={{minWidth: "300px"}}>
+                <Col className="d-flex justify-content-center">
+                    <Button className={`w-100 ${st.h100} ${st.borderRadius} ${st.loginButton}`}>회원가입 하기</Button>
+                </Col>
+            </Row>
+
         </Container>
     );
 };
