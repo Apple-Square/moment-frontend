4fdc4455ea3814d81ca9746038dce5637171c19a
rjsdn031 <rjsdn031@naver.com>
2024-08-17 20:47:10 +0900

    test: 테스트 피드 데이터 FeedList에 추가 테스트 피드 데이터 FeedList에 추가('홍길동','','테스트다!') Related to: #9

--- src/pages/feed/FeedList.tsx
+++ src/pages/feed/FeedList.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import Feed from './Feed';
 
 const FeedList:React.FC = () => {
     return (
         <div>
-
+            <Feed
+                author='홍길동'
+                img=''
+                contents='테스트다!'
+            />
         </div>
     );
 };
