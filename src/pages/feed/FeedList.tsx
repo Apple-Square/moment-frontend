5b0b13641c0020f4f6a7b12b4f565f6c20373571
rjsdn031 <rjsdn031@naver.com>
2024-08-17 21:07:09 +0900

    design: FeedList bootstrap 지정 FeedList bootstrap(container) 지정 Related to: #9

--- src/pages/feed/FeedList.tsx
+++ src/pages/feed/FeedList.tsx
@@ -1,15 +1,19 @@
 import React from 'react';
-import Feed from './Feed';
+import Feed from './components/Feed';
+import styles from "./css/FeedList.module.css";
+import { Container } from "react-bootstrap";
 
-const FeedList:React.FC = () => {
+
+const FeedList: React.FC = () => {
     return (
-        <div>
+        <Container className={`${styles.container}`}>
             <Feed
                 author='홍길동'
                 img=''
                 contents='테스트다!'
             />
-        </div>
+        </Container>
+
     );
 };
 
