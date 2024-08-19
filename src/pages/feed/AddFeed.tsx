4748118ed8a81cfe7049d4fdd1911a1c37748303
rjsdn031 <rjsdn031@naver.com>
2024-08-19 17:57:34 +0900

    feat: AddFeed 추가 AddFeed 컴포넌트 수정 AddFeed css 모듈 생성 useState 정의 Fixes: #9

--- src/pages/feed/AddFeed.tsx
+++ src/pages/feed/AddFeed.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from "./css/AddFeed.module.css";
+import { Button, Container } from 'react-bootstrap';
 
-const AddFeed:React.FC = () => {
+const AddFeed: React.FC = () => {
+    const [image, setImage] = useState('');
+    const [contents, setContents] = useState('');
+
+    const handleImageUpload = () => {
+        setImage(image);
+    }
+
+    const handleUpload = () => {
+        setContents(contents);
+    }
+
     return (
-        <div>
-
-        </div>
+        <Container className={styles.container}>
+            <div className={styles.imageContainer}>
+                <img className={styles.uploadImage} />
+            </div>
+            <div className={styles.formContainer}>
+                <form></form>
+            </div>
+            <div className={styles.buttonBox}>
+                <Button>위치</Button>
+                <Button>완료하기</Button>
+            </div>
+        </Container>
     );
 };
 
