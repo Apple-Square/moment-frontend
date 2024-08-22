fc8c6c96234e804f5867ef432f1b316a966e7d63
rjsdn031 <rjsdn031@naver.com>
2024-08-23 04:41:44 +0900

    feat: AddFeed Page에서 이미지 미리보기 지원 이미지 올리면 올린 이미지 미리보기 추가 Fixes: #9

--- src/pages/feed/components/ImageUploader.tsx
+++ src/pages/feed/components/ImageUploader.tsx
@@ -1,20 +1,33 @@
-import React from "react";
+import React, { useState } from 'react';
 import styles from "../css/ImageUploader.module.css";
 
 interface ImageUploaderProps {
-    onImageChange: (file: File) => void;
+    onImageChange: (file: File|null) => void;
 }
 
 const ImageUploader: React.FC<ImageUploaderProps> = ({ onImageChange }) => {
+    const [preview, setPreview] = useState<string|null>(null);
+
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
             onImageChange(event.target.files[0]);
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setPreview(reader.result as string);
+            };
+            reader.readAsDataURL(event.target.files[0]);
+        } else {
+            setPreview(null);
+            onImageChange(null);
         }
     };
 
     return (
         <div className={styles.imageUploader}>
             <input type="file" accept="image/*" onChange={handleFileChange} />
+            { preview && (
+                <img src={preview} alt="preview" className={styles.imagePreview} />
+            )}
         </div>
     );
 };
