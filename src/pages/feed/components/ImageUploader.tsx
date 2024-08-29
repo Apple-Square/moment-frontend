36d315f4892a8f296123d74c2c0cb8c7fb929022
rjsdn031 <rjsdn031@naver.com>
2024-08-30 02:28:23 +0900

    comment: ImageUploader 주석 설정 및 코드 스타일 정리

--- src/pages/feed/components/ImageUploader.tsx
+++ src/pages/feed/components/ImageUploader.tsx
@@ -2,7 +2,7 @@ import React, { useRef, useState } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import SwiperCore from 'swiper';
 import styles from "../css/ImageUploader.module.css";
-import  DelImg from "../../../assets/close.svg";
+import DelImg from "../../../assets/close.svg";
 import 'swiper/css';
 
 interface ImageUploaderProps {
@@ -28,7 +28,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ contents, onImageChange }
         // preview(string[])에서 제외
         const updatedPreviewList = previewList.filter((_, idx) => idx !== index);
         setPreviewList(updatedPreviewList);
-    } 
+    }
 
     const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         if (event.target.files && event.target.files[0]) {
@@ -39,6 +39,7 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ contents, onImageChange }
             }
             onImageChange(imgArray.slice(0, 10));
 
+            // bug: 용량이 큰 이미지가 뒤로 가게 됨
             const newPreviews: string[] = [];
             imgArray.forEach((file) => {
                 const reader = new FileReader();
@@ -67,48 +68,48 @@ const ImageUploader: React.FC<ImageUploaderProps> = ({ contents, onImageChange }
     };
 
     return (
-        <>
-            <div className={styles.imageUploader}>
-                <input  // hidden tag
-                    type="file"
-                    accept="image/*"
-                    multiple
-                    ref={fileInputRef}
-                    onChange={handleFileChange}
-                />
-                <Swiper
-                    className={styles.prevSlide}
-                    style={{ position: 'absolute' }}      // css bug point
-                    slidesPerView={1}
-                    onSlideChange={() => console.log('slide change')}
-                    onSwiper={(swiper) => {
-                        swiperRef.current = swiper;
-                        console.log(swiper);
-                    }}
-                >
-                    {previewList && (
-                        previewList.map((preview, index) => (
-                            <SwiperSlide key={index}>
-                                <div
-                                    className={styles.imagePreview}
-                                    style={{
-                                        backgroundImage: `url(${preview})`,
-                                    }}
-                                ></div>
-                                <img src={DelImg} className={styles.deleteBtn} onClick={() => handleDeleteImage(index)} />
-                            </SwiperSlide>
-                        ))
-                    )}
-                    {(previewList.length < 10) && (
-                        <SwiperSlide>
-                            <div className={styles.imagePreview}>
-                                <button className={styles.uploadBtn} onClick={handleUploadClick}>이미지 업로드</button>
-                            </div>
+        <div className={styles.imageUploader}>
+            <input  // hidden tag
+                type="file"
+                accept="image/*"
+                multiple
+                ref={fileInputRef}
+                onChange={handleFileChange}
+            />
+            {/* swipper setting */}
+            <Swiper
+                className={styles.prevSlide}
+                style={{ position: 'absolute' }}      // css bug point
+                slidesPerView={1}
+                onSlideChange={() => console.log('slide change')}
+                onSwiper={(swiper) => {
+                    swiperRef.current = swiper;
+                    console.log(swiper);
+                }}
+            >
+                {previewList && (
+                    previewList.map((preview, index) => (
+                        <SwiperSlide key={index}>
+                            <div
+                                className={styles.imagePreview}
+                                style={{
+                                    backgroundImage: `url(${preview})`,
+                                }}
+                            ></div>
+                            {/* svg 이미지 변경 필요 */}
+                            <img src={DelImg} className={styles.deleteBtn} onClick={() => handleDeleteImage(index)} />
                         </SwiperSlide>
-                    )}
-                </Swiper>
-            </div>
-        </>
+                    ))
+                )}
+                {(previewList.length < 10) && (
+                    <SwiperSlide>
+                        <div className={styles.imagePreview}>
+                            <button className={styles.uploadBtn} onClick={handleUploadClick}>이미지 업로드</button>
+                        </div>
+                    </SwiperSlide>
+                )}
+            </Swiper>
+        </div>
     );
 };
 
