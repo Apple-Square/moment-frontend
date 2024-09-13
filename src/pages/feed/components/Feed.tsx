8987c948eb5ff7eac729740355ee5773be183ffc
rjsdn031 <rjsdn031@naver.com>
2024-09-13 22:17:48 +0900

    fix: 좋아요 버튼 디자인 및 클릭 이벤트 설정
    좋아요 버튼 디자인 수정
    클릭 이벤트 설정(서버 미연동)
    Fixes: #9

--- src/pages/feed/components/Feed.tsx
+++ src/pages/feed/components/Feed.tsx
@@ -7,6 +7,7 @@ import { Pagination } from 'swiper/modules';
 import 'swiper/css/pagination';
 // import SwiperCore from 'swiper';
 import 'swiper/css';
+import SvgLike from './SvgLike';
 // import { Col, Container, Row } from 'react-bootstrap';
 
 interface FeedProps {
@@ -23,12 +24,14 @@ interface FeedProps {
 
 const Feed: React.FC<FeedProps> = ({ profileImg, author, location, images, contents, likes, comments, shares, timeAgo }) => {
     const [visibleComment, setVisibleComment] = useState<boolean>(false);
+    const [liked, setLiked] = useState<boolean>(false);
+
     const navi = useNavigate();
     const locationPath = useLocation();
     const pagination = {
         clickable: false,
         renderBullet: function (index:number, className:string) {
-          return '<span class="' + className + '"></span>';
+          return `<span className="${className}">${index}</span>`;
         },
     };
 
@@ -43,6 +46,12 @@ const Feed: React.FC<FeedProps> = ({ profileImg, author, location, images, conte
     const handleClickComment = () => {
         // setVisibleComment(!visibleComment);
         navi('/feed/FeedDetail');
+    }
+
+    const handleClickLike = () => {
+        setLiked(!liked);
+        // post like (liked ? 1 : -1)
+        // get like
     }
 
     return (
@@ -77,8 +86,11 @@ const Feed: React.FC<FeedProps> = ({ profileImg, author, location, images, conte
                     
                 </Swiper>
             </div>
-            <div className={styles.actions}>
-                <span className={styles.likes}>👍 {likes}</span>
+            <div className={`${styles.actions} px-1`}>
+                <span className={styles.likes}><SvgLike className={styles.likeBtn} onClick={handleClickLike} style={{
+                                                                                        "width": "1rem",
+                                                                                        "height": "auto"
+                                                                                    }} fill={liked ? "blue" : "black"}/> {likes}</span>
                 <span className={styles.comments} onClick={handleClickComment}>💬 {comments}</span>
                 <span className={styles.shares}>↗️ {shares}</span>
                 <span className={styles.timeAgo}>{timeAgo}</span>
